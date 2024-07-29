ce3fcd3f8ca693ce1bcfa8839525bd6a29dc42b4
Satosan2001 <168102457+Satosan2001@users.noreply.github.com>
2024-07-29 11:54:12 +0100

    Update main.js

--- Terminal Website/js/main.js
+++ Terminal Website/js/main.js
@@ -56,7 +56,7 @@ function enterKey(e) {
     if (e.keyCode == 13) {
       commands.push(command.innerHTML);
       git = commands.length;
-      addLine("visitor@artfulwall.art:~$ " + command.innerHTML, "no-animation", 0);
+      addLine("visitor@salahboussettah.online:~$ " + command.innerHTML, "no-animation", 0);
       commander(command.innerHTML.toLowerCase());
       command.innerHTML = "";
       textarea.value = "";
